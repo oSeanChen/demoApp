76e1c046333be13300ee0ddae7aa721c3a6fa697
Sean <abbb123p@gmail.com>
2024-05-31 01:05:00 +0800

    加入 tailwindCSS 及調整頁面

--- src/main/java/com/oseanchen/demoapp/controller/UserController.java
+++ src/main/java/com/oseanchen/demoapp/controller/UserController.java
@@ -3,6 +3,7 @@ package com.oseanchen.demoapp.controller;
 import com.oseanchen.demoapp.dto.UserRegisterRequest;
 import com.oseanchen.demoapp.model.User;
 import com.oseanchen.demoapp.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,8 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Optional;
 
 @Controller
 public class UserController {
@@ -41,10 +40,16 @@ public class UserController {
         User user = userService.getUserByEmail(email);
         if (user != null) {
             model.addAttribute("message", "Login successful");
-            return "success";  // 返回 success.html 視圖
+            model.addAttribute("email", user.getEmail());
+            return "home";  // 返回 home.html 視圖
         }
         model.addAttribute("message", "Invalid credentials");
         return "login";  // 返回 login.html 視圖
     }
 
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(HttpSession session) {
+        session.invalidate();  // 使 session 失效
+        return ResponseEntity.ok().build();
+    }
 }
